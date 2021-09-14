7052571f0c951977ef591cf4cb53eadcf4ea1c45
Stormboys3 <89860909+Stormboys3@users.noreply.github.com>
2021-09-14 14:56:58 -0500

    Add files via upload

---
+++ Maths.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Maths
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Modulo (%) says how many times a
+            //number fits into an another number
+            //If it prints 0 it's even
+            //1 if it's odd
+            int num1 = 64;
+            int num2 = 8;
+            int DividedNum = num1 % num2;
+            int AbNum = -8;
+            int SqrtNum = 128;
+            //Dont forget the m
+            decimal RoundNum = 48.658m;
+            double MinNum = 0.965;
+            int MaxNum = 360;
+            int funy = 0;
+
+            Console.WriteLine(DividedNum);
+
+            //Math.Abs() finds the absolute value of a number
+            Console.WriteLine(Math.Abs(AbNum));
+
+            //Math.Pow() raises #1 by the power of #2
+            Console.WriteLine(Math.Pow(10, 1000));
+
+            //Math.Sqrt() finds the square root of a number
+            Console.WriteLine(Math.Sqrt(SqrtNum));
+
+            //Math.Ceiling() rounds up decimals and doubles
+            Console.WriteLine(Math.Ceiling(RoundNum));
+
+            //Math.Floor() rounds down decimals and doubles
+            Console.WriteLine(Math.Floor(RoundNum));
+
+            //Math.Min() finds the smaller number of 2
+            Console.WriteLine(Math.Min(MinNum, 36));
+
+            //Math.Max() finds the bigger number of 2
+            Console.WriteLine(Math.Max(MaxNum, 25));
+
+            //You can use multiple methods at once
+            Console.WriteLine(Math.Floor(Math.Max(7.84, 52.652)));
+
+            //Increments and decrements +1 is ++ and -1 is -- more than that is
+            //+= and -=
+            funy += 2;
+            funy--;
+            Console.WriteLine(funy);
+
+            //Math.Round() rounds doubles up
+            Console.WriteLine(Math.Round(6.98562));
+            Console.WriteLine(Math.Round(3.14469823, 2));
+        }
+    }
+}
---
+++ Methods.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            LeMethod();
+            ParameterMethod(8);
+            VariableMethod("no");
+            MoreVariables(a: 30000, b: 7000, c: 600, d: 40, e: 3);
+            Re(word1: "Hello ", word2: "hooman.");
+            Re(word1: "How ", word2: "you ", word3: "do?");
+            Re();
+            string output = Whisper("OK.");
+            Console.WriteLine(output);
+        }
+        static void LeMethod()
+        {
+            Console.WriteLine("It worked.");
+        }
+        //The variable can only be used inside the method.
+        static void ParameterMethod(int num)
+        {
+            
+            Console.WriteLine($"My life savings is {num} dollars.");
+        }
+        static void VariableMethod(string msg, string dot = ".")
+        {
+            Console.WriteLine(msg + dot);
+        }
+        static void MoreVariables(int a, int b, int c, int d, int e)
+        {
+            Console.WriteLine(a + b + c + d + e);
+        }
+        //An overload is when a method has the same name, but with different numbers of variables.
+        static void Re(string word1, string word2)
+        {
+            Console.WriteLine(word1 + word2);
+        }
+        static void Re(string word1, string word2, string word3)
+        {
+            Console.WriteLine(word1 + word2 + word3);
+        }
+        static void Re()
+        {
+            Console.WriteLine("Nothing.");
+        }
+        //Returns a string, so it has the string modifier right before the name.
+        static string Whisper(string phrase)
+        {
+            //Return tells the computer to exit the method and return a value to wherever
+            //the method was called.
+
+            return phrase.ToLower();
+        }
+    }
+}
