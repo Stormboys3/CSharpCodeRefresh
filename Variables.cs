7684188af1a9c2f2fc81c262320f8267383dedca
Stormboys3 <89860909+Stormboys3@users.noreply.github.com>
2021-09-14 07:47:35 -0500

    Add files via upload

---
+++ Variables.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // variables
+            int num1 = 7;
+            int num2 = num1 + 3;
+            int num3 = num1 + num2;
+            int funnyNumber = 300;
+            string Word = "string of text";
+            double OtherNumber = 150.65;
+            bool DaTruth = true;
+            // dont forget the m idiot
+            decimal SmolNum = 159.46m;
+            int answer = 8 + (9 * 3);
+
+            Console.WriteLine(SmolNum);
+            Console.WriteLine(funnyNumber);
+            Console.WriteLine(num3);
+            Console.WriteLine(OtherNumber);
+            Console.WriteLine(answer);
+            Console.WriteLine(Word);
+            Console.WriteLine(DaTruth);
+            
+            
+        }
+    }
+}
