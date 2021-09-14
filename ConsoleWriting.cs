a15c0d2e935bc163c0c400a14cda3ef8b81cb12f
Stormboys3 <89860909+Stormboys3@users.noreply.github.com>
2021-09-14 07:39:22 -0500

    Add files via upload

---
+++ ConsoleWriting.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ComputerSaysVeryNice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Very Nice");
+        }
+    }
+}
